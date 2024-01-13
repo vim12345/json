01fff87047836679ee2ed0ebb08a93bd5aed388c
vim12345 <vk2699945@gmail.com>
2024-01-14 02:23:31 +0530

    new

---
+++ json.js
@@ -0,0 +1,15 @@
+const data = {
+    "name": "Vimal Kumar",
+    "age": 23,
+    "hobby": {
+        "reading": true,
+        "gaming": false,
+        "sport": "cricket"
+    },
+    "class": ["javascript", "html", "css"]
+}
+
+console.log(data.name);
+console.log(data.hobby);
+console.log(data.hobby.sport);
+console.log(data.class[1]);
